1a9b76c02a325485a7cca286df934b3b403a1e95
Aksellofstedtnielsen <aksel98@outlook.com>
2023-11-17 12:50:54 +0100

    Rebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/welcome-screen.dart';
+import 'package:todo_app/screens/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: WelcomeScreen(),
+      title: 'Todo App',
+      home: Home(),
     );
   }
 }
