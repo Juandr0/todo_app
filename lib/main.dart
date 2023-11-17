6a729033e3844d68cf93595666075893bb35cc82
Aksellofstedtnielsen <aksel98@outlook.com>
2023-11-17 14:47:23 +0100

    Adding image to welcome

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Todo App',
-      home: Home(),
+      home: WelcomeScreen(),
     );
   }
 }
