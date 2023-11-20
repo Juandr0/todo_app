8bcdd0779e03a97c346a40c4a4f3b026dfa8c215
Aksellofstedtnielsen <aksel98@outlook.com>
2023-11-20 14:32:35 +0100

    Scrollable list

--- lib/screens/welcome-screen.dart
+++ lib/screens/welcome-screen.dart
@@ -1,9 +1,23 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/app_sizes.dart';
 import 'package:todo_app/constants/colors.dart';
 import 'package:todo_app/shared-preferences.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class ImageData {
+  final String imagePath;
+  final String description;
+
+  ImageData(this.imagePath, this.description);
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   final Color buttonColor = darkGrey;
   final Color textColor = lightGrey;
   final double titleFontSize = AppSizes.titleText;
@@ -14,7 +28,14 @@ class WelcomeScreen extends StatelessWidget {
   final double buttonSpacing = AppSizes.between * 2;
   final BorderRadius buttonBorderRadius = BorderRadius.circular(8.0);
 
-  WelcomeScreen({super.key});
+  final List<ImageData> imageData = [
+    ImageData('assets/note.png', 'This is the first image blabla'),
+    ImageData('assets/note.png', 'Second image bloblo'),
+    ImageData('assets/note.png', 'Third image hoho'),
+  ];
+
+  int _selectedIndex = -1;
+  bool _showDescription = false;
 
   Widget buildTitleSection() {
     return Expanded(
@@ -45,24 +66,70 @@ class WelcomeScreen extends StatelessWidget {
     );
   }
 
-  Widget buildImageSection(BuildContext context) {
+  Widget buildImageSlider(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Image.asset(
-          'path',
+      children: [
+        SizedBox(
           height: 200.0,
-          width: 200.0,
-          fit: BoxFit.cover,
-        ),
-        SizedBox(height: 10.0),
-        const Text(
-          'description',
-          style: TextStyle(
-            fontSize: 16.0,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: imageData.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: 250.0,
+                        width: 250.0,
+                        decoration: BoxDecoration(
+                          border: _selectedIndex == index
+                              ? Border.all(color: Colors.blue, width: 2.0)
+                              : null,
+                        ),
+                        child: ClipRect(
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(
+                              sigmaX: _selectedIndex == index ? 5.0 : 0.0,
+                              sigmaY: _selectedIndex == index ? 5.0 : 0.0,
+                            ),
+                            child: Image.asset(
+                              imageData[index].imagePath,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                      if (_selectedIndex == index)
+                        Positioned.fill(
+                          child: Container(
+                            padding: EdgeInsets.all(8.0),
+                            child: Center(
+                              child: Text(
+                                imageData[index].description,
+                                style: const TextStyle(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                textAlign: TextAlign.center,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            },
           ),
         ),
       ],
@@ -122,9 +189,9 @@ class WelcomeScreen extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          buildTitleSection(),
-          buildImageSection(context),
-          buildButtonSection(context),
+          // buildTitleSection(),
+          buildImageSlider(context),
+          //buildButtonSection(context),
         ],
       ),
     );
