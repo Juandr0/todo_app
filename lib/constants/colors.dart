774a837761bc5e899ed32b581c2236aae284b434
Aksellofstedtnielsen <aksel98@outlook.com>
2023-11-21 16:54:03 +0100

    some colors and sizes

---
+++ lib/constants/colors.dart
@@ -0,0 +1,5 @@
+00);
+
+const Color selectedColor = Color.fromARGB(255, 212, 33, 243);
+const Color passiveColor = Color.fromARGB(255, 86, 86, 86);
+const Color descriptionTextColor = Color.fromARGB(255, 0, 136, 43);
