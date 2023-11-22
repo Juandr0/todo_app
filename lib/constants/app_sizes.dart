855ff02a40ace25d4a68383cc72dc65ab3a39533
Aksellofstedtnielsen <aksel98@outlook.com>
2023-11-22 15:39:28 +0100

    Updated sizes

--- lib/constants/app_sizes.dart
+++ lib/constants/app_sizes.dart
@@ -4,12 +4,4 @@ class AppSizes {
   static const inline = 5.0;
   static const within = 10.0;
   static const between = 20.0;
-
-  static const titleText = 32.0;
-  static const subtitleText = 18.0;
-  static const buttonText = 16.0;
-  static const borderRadius = 8.0;
-  static const imageBoxHeight = 250.0;
-  static const imageBoxWidht = 200.0;
-  static const imageBorder = 2.0;
 }
---
+++ lib/models/image-data.dart
@@ -0,0 +1,6 @@
+class ImageData {
+  final String imagePath;
+  final String description;
+
+  ImageData(this.imagePath, this.description);
+}
